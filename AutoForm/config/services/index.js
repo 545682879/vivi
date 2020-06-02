2408adb90315cde72fe2454c97c5dd48f5b7f043
vivichen <17105@etransfar.com>
2020-06-02 12:20:29 +0800

    add

--- AutoForm/config/services/index.js
+++
@@ -1,7 +0,0 @@
-import createService from '@/utils/createService';
-
-export default createService({
-  create: 'post /otm/pageTemplate/create',
-  tables: 'get /otm/table/list',
-  tablecolumns: 'get /otm/table/listTableColumn',
-});
--- AutoForm/src/config/readme.txt
+++
@@ -1,8 +0,0 @@
-创建页面:
-/config/create
-测试页面
-/config/test?templateName=template20200318|S,template20200319|F,template20200320|T
-
-S-查询
-F-表单（默认）
-T-表格
